388eccc9a7c7e5cdbc4e3e86436fb522154944f3
Gustavo Zembruski <151480220+goolgeima@users.noreply.github.com>
2025-09-06 21:49:36 -0300

    Update and rename tarefasordenadas.java to Problema1TarefasOrdenadas.java

--- Problema1TarefasOrdenadas.java
+++ Problema1TarefasOrdenadas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class tarefasordenadas{
+public class Problema1TarefasOrdenadas{
     public static void main(String[] args) {
     
         Scanner sc  = new Scanner(System.in);
